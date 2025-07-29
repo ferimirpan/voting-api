c310877396f22c772257194b670da8062a071dea
ferimirpan <ferimirpan@gmail.com>
2025-07-29 13:17:41 +0700

    add unit test for service auth

--- app/tests/auth.test.js
+++ app/tests/auth.test.js
@@ -3,6 +3,8 @@ import request from 'supertest';
 import { MongoMemoryServer } from 'mongodb-memory-server';
 import { connectDB, disconnectDB } from '../config/db.js';
 import 'dotenv/config';
+import Token from '../models/token.model.js';
+import User from '../models/user.model.js';
 
 let mongoServer;
 
@@ -25,15 +27,15 @@ describe('registerUser', () => {
       .send()
       .set('Accept', 'application/json');
 
-    expect(res.statusCode).toBe(400);
+    expect(res.statusCode).toBe(422);
     expect(res.body).toHaveProperty('message', 'password is required,email is required,fullName is required');
   });
 
 
   it('should be throw error when email inputed not use mail format', async () => {
     const params = {
-      fullName: 'User 1',
-      email: 'user1',
+      fullName: 'User 101',
+      email: 'user101',
       password: 'pass1234!',
       passwordConfirmation: 'pass1234!'
     }
@@ -42,7 +44,7 @@ describe('registerUser', () => {
       .send(params)
       .set('Accept', 'application/json');
 
-    expect(res.statusCode).toBe(400);
+    expect(res.statusCode).toBe(422);
     expect(res.body).toHaveProperty('message', 'input must be in email format');
   });
 
@@ -58,13 +60,13 @@ describe('registerUser', () => {
       .send(params)
       .set('Accept', 'application/json');
 
-    expect(res.statusCode).toBe(400);
+    expect(res.statusCode).toBe(422);
   });
 
   it('should be throw error when the password and passwordConfirmation not match', async () => {
     const params = {
-      fullName: 'User 99',
-      email: 'user99@vote.com',
+      fullName: 'User 101',
+      email: 'user101@vote.com',
       password: 'pass1234!',
       passwordConfirmation: 'pass1234'
     }
@@ -73,14 +75,14 @@ describe('registerUser', () => {
       .send(params)
       .set('Accept', 'application/json');
 
-    expect(res.statusCode).toBe(400);
+    expect(res.statusCode).toBe(422);
     expect(res.body).toHaveProperty('message', 'password does not match');
   });
 
-  it('should be registered successfully', async () => {
+  it('registered successfully', async () => {
     const params = {
-      fullName: 'User 101',
-      email: 'user101@vote.com',
+      fullName: 'User 100',
+      email: 'user100@vote.com',
       password: 'pass1234!',
       passwordConfirmation: 'pass1234!'
     }
@@ -91,5 +93,131 @@ describe('registerUser', () => {
 
     expect(res.statusCode).toBe(201);
     expect(res.body).toHaveProperty('data');
+
+    await User.deleteOne({
+      _id: res.body.data._id,
+    });
+  });
+});
+
+describe('login', () => {
+  it('should be throw error when email and password is null', async () => {
+    const res = await request(app)
+      .post('/api/v1/auth/login')
+      .send()
+      .set('Accept', 'application/json');
+
+    expect(res.statusCode).toBe(422);
+    expect(res.body).toHaveProperty('message', 'email and password required');
+  });
+
+  it('should be throw error when email invalid', async () => {
+    const params = {
+      email: 'user0@vote.com',
+      password: 'pass1234!',
+    }
+
+    const res = await request(app)
+      .post('/api/v1/auth/login')
+      .send(params)
+      .set('Accept', 'application/json')
+      .set('decryptedPass', 'true');
+
+    expect(res.statusCode).toBe(422);
+    expect(res.body).toHaveProperty('message', 'email or password invalid');
+  });
+
+  it('should be throw error when password incorrect', async () => {
+    const params = {
+      email: 'user101@vote.com',
+      password: 'pass1234',
+    }
+
+    const res = await request(app)
+      .post('/api/v1/auth/login')
+      .send(params)
+      .set('Accept', 'application/json')
+      .set('decryptedPass', 'true');
+
+    expect(res.statusCode).toBe(422);
+    expect(res.body).toHaveProperty('message', 'email or password invalid');
+  });
+
+  it('login successfully', async () => {
+    const params = {
+      email: 'user101@vote.com',
+      password: 'pass1234!',
+    }
+
+    const res = await request(app)
+      .post('/api/v1/auth/login')
+      .send(params)
+      .set('Accept', 'application/json')
+      .set('decryptedPass', 'true');
+
+    expect(res.statusCode).toBe(200);
+    expect(res.body).toHaveProperty('data');
+  });
+});
+
+describe('loginData', () => {
+  it('get login data failed', async () => {
+    const getToken = await Token.findOne().sort({
+      createdAt: -1
+    });
+
+    const res = await request(app)
+      .get('/api/v1/auth/loginData')
+      .send()
+      .set('Accept', 'application/json')
+      .set('Authorization', getToken.token + '123456789');
+
+    expect(res.statusCode).toBe(401);
+  });
+
+  it('get login data successfully', async () => {
+    const getToken = await Token.findOne().sort({
+      createdAt: -1
+    });
+
+    const res = await request(app)
+      .get('/api/v1/auth/loginData')
+      .send()
+      .set('Accept', 'application/json')
+      .set('Authorization', getToken.token);
+
+    expect(res.statusCode).toBe(200);
+    expect(res.body).toHaveProperty('data');
+  });
+});
+
+describe('logout', () => {
+  it('logout failed', async () => {
+    const getToken = await Token.findOne().sort({
+      createdAt: -1
+    });
+
+    const res = await request(app)
+      .post('/api/v1/auth/logout')
+      .send()
+      .set('Accept', 'application/json')
+      .set('Authorization', getToken.token + '123456789');
+
+    expect(res.statusCode).toBe(401);
+  });
+
+  it('logout successfully', async () => {
+    const getToken = await Token.findOne().sort({
+      createdAt: -1
+    });
+
+    const res = await request(app)
+      .post('/api/v1/auth/logout')
+      .send()
+      .set('Accept', 'application/json')
+      .set('Authorization', getToken.token);
+
+    expect(res.statusCode).toBe(200);
+    expect(res.body).toHaveProperty('message', 'logout successfully');
   });
 });
